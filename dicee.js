388bba11ff0704d9b5eeaa39b7d496362ff61716
thedhanrajsingh <57003743+thedhanrajsingh@users.noreply.github.com>
2020-03-29 10:50:16 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,19 @@
+// left side image
+var randomNumber1 = Math.floor(Math.random() * 6);
+var randomImageArray = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+document.querySelector(".img1").setAttribute("src", randomImageArray[randomNumber1]);
+
+
+//  Right side image
+var randomNumber2 = Math.floor(Math.random() * 6);
+document.querySelector(".img2").setAttribute("src", randomImageArray[randomNumber2]);
+
+if (randomNumber1>randomNumber2) {
+   document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+else {
+    document.querySelector("h1").innerHTML="Draw";
+}
